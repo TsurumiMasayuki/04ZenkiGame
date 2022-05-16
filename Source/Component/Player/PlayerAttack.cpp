ff0de48ba1937c7f0991bbdd66aa388c7d2114f3
鶴見昌之 <tsuruwatcher@gmail.com>
2022-05-16 14:55:44 +0900

    コントローラー対応とプレイヤー攻撃クラスの作成

--- Source/Component/Player/PlayerAttack.cpp
+++ Source/Component/Player/PlayerAttack.cpp
@@ -11,6 +11,8 @@
 
 #include "Component/Player/PlayerStats.h"
 
+#include "Device/GameInput.h"
+
 #include "Effect/TestFlameEffect.h"
 #include "Effect/TestVibrationEffect.h"
 
@@ -19,10 +21,11 @@
 void PlayerAttack::onStart()
 {
 	//自身にコライダーをアタッチ
-	auto pBoxCollider = getUser().addComponent<BoxColiiderBt>();
-	pBoxCollider->setMass(1.0f);
-	pBoxCollider->setTrigger(true);
-	pBoxCollider->setUseGravity(false);
+	m_pBoxCollider = getUser().addComponent<BoxColiiderBt>();
+	m_pBoxCollider->setMass(1.0f);
+	m_pBoxCollider->setTrigger(true);
+	m_pBoxCollider->setUseGravity(false);
+	m_pBoxCollider->setActive(false);
 
 	//カメラがアタッチされているオブジェクトを取得
 	auto pCameraObject = &getUser().getGameMediator()->getMainCamera()->getUser();
@@ -38,6 +41,43 @@ void PlayerAttack::onStart()
 
 void PlayerAttack::onUpdate()
 {
+	//入力を取得
+	bool input = GameInput::getInstance().getPlayerDash();
+
+	//入力されていたら
+	if (input)
+	{
+		if (m_pPlayerActionManager->actionCount() == 0)
+		{
+			//間隔を開けて火炎エフェクト
+			auto pSequence = new Action::Sequence(
+				{
+					new Action::TestFlameEffect(m_pPlayerActionManager),
+					new Action::WaitForSeconds(0.5f)
+				}
+			);
+
+			//リピート実行
+			auto pRepeat = new Action::RepeatForever(pSequence);
+
+			m_pPlayerActionManager->enqueueAction(pRepeat);
+		}
+
+		//コライダーを有効化
+		m_pBoxCollider->setActive(true);
+	}
+	else
+	{
+		//エフェクトを停止
+		m_pPlayerActionManager->forceNext();
+		//コライダーを無効化
+		m_pBoxCollider->setActive(false);
+	}
+}
+
+void PlayerAttack::init(Action::ActionManager* pPlayerActionManager)
+{
+	m_pPlayerActionManager = pPlayerActionManager;
 }
 
 void PlayerAttack::onCollisionEnter(GameObject* pHit)
